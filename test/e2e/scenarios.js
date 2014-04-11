a8ed32181ca0c116827257f802862bcf91d335e9
Jamie Carter <jcarter3984@gmail.com>
2014-04-11 12:13:31 -0500

    updated unit and e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,6 +2,7 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/getting-started.md */
 
+
 describe('my app', function() {
 
   browser.get('index.html');
@@ -12,6 +13,7 @@ describe('my app', function() {
 
 
   describe('view1', function() {
+	var htmlElement;
 
     beforeEach(function() {
       browser.get('index.html#/view1');
@@ -19,9 +21,93 @@ describe('my app', function() {
 
 
     it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
-    });
+      expect(element.all(by.css('[ng-view] form')).first().getAttribute('id')).
+        toMatch(/frmEdit/);
+    });	
+	
+		
+	it('should have a "Make:" label', function() {
+		htmlElement = element(by.id('lblMake'));
+		expect(htmlElement).not.toBe(undefined);
+		expect(htmlElement.getText()).toEqual('Make:');
+	});
+	
+	it('should have a "Make" edit', function() {
+		htmlElement = element(by.id('edMake'));
+		expect(htmlElement).not.toBe(undefined);
+		expect(htmlElement.getAttribute('value')).toEqual('Honda');
+	});	
+	
+	it('should have a "Make" span', function() {
+		htmlElement = element(by.id('spMake'));
+		var makeEdit = element(by.id('edMake'));
+		makeEdit.clear();
+		makeEdit.sendKeys('Nissan');
+		expect(makeEdit.getAttribute('value')).toEqual('Nissan');
+		expect(htmlElement.getText()).toEqual('Nissan');
+	});	
+
+	it('should have a "Model:" label', function() {
+		htmlElement = element(by.id('lblModel'));
+		expect(htmlElement).not.toBe(undefined);
+		expect(htmlElement.getText()).toEqual('Model:');
+	});
+	
+	it('should have a "Model" edit', function() {
+		htmlElement = element(by.id('edModel'));
+		expect(htmlElement).not.toBe(undefined);
+		expect(htmlElement.getAttribute('value')).toEqual('Civic');
+	});	
+	
+	it('should have a "Model" span', function() {
+		htmlElement = element(by.id('spModel'));
+		var modelEdit = element(by.id('edModel'));
+		modelEdit.clear();
+		modelEdit.sendKeys('Frontier');
+		expect(modelEdit.getAttribute('value')).toEqual('Frontier');
+		expect(htmlElement.getText()).toEqual('Frontier');
+	});
+	
+	it('should have a "Year:" label', function() {
+		htmlElement = element(by.id('lblYear'));
+		expect(htmlElement).not.toBe(undefined);
+		expect(htmlElement.getText()).toEqual('Year:');
+	});
+	
+	it('should have a "Year" edit', function() {
+		htmlElement = element(by.id('edYear'));
+		expect(htmlElement).not.toBe(undefined);
+		expect(htmlElement.getAttribute('value')).toEqual('2014');
+	});	
+	
+	it('should have a "Year" span', function() {
+		htmlElement = element(by.id('spYear'));
+		var yearEdit = element(by.id('edYear'));
+		yearEdit.sendKeys('2012');
+		expect(yearEdit.getAttribute('value')).toEqual('2012');
+		expect(htmlElement.getText()).toEqual('2012');
+	});
+
+	it('should have a "Price:" label', function() {
+		htmlElement = element(by.id('lblPrice'));
+		expect(htmlElement).not.toBe(undefined);
+		expect(htmlElement.getText()).toEqual('Price:');
+	});
+	
+	it('should have a "Price" edit', function() {
+		htmlElement = element(by.id('edPrice'));
+		expect(htmlElement).not.toBe(undefined);
+		expect(htmlElement.getAttribute('value')).toEqual('24000');
+	});	
+	
+	it('should have a "Price" span', function() {
+		htmlElement = element(by.id('spPrice'));
+		var PriceEdit = element(by.id('edPrice'));
+		PriceEdit.clear();
+		PriceEdit.sendKeys('20000');
+		expect(PriceEdit.getAttribute('value')).toEqual('20000');
+		expect(htmlElement.getText()).toEqual('$20,000.00');
+	});			
 
   });
 
@@ -34,9 +120,19 @@ describe('my app', function() {
 
 
     it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
+      expect(element.all(by.css('[ng-view] th')).first().getText()).
+        toMatch(/Make/);		
+	});
+	
+	it('should have a list of cars and should have the correct number of rows', function() {
+		var carsList = element.all(by.repeater('car in cars'));
+		expect(carsList.count()).toBe(6);
+	});
+	
+	it('should have "Honda" as the make in the first row', function() {
+		var carsList = element.all(by.repeater('car in cars').column('{{car.make}}')).get(0);
+		expect(carsList.getText()).toEqual('Honda');
+	});	
+	
   });
 });
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,14 +3,72 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
+	describe('MyCtrl1', function(){
+		var scope = {}, ctrl;
+		
   beforeEach(module('myApp.controllers'));
+  
+  beforeEach(inject(function($controller) {
+	scope = {};
+	ctrl = $controller('MyCtrl1',{$scope: scope});
+	}));
 
 
-  it('should ....', inject(function() {
-    //spec body
+  it('should have a "car" property',(function() {
+		expect(scope.car)
+  }));
+  
+  it('should have a "car.make" property',(function() {
+		expect(scope.car.make)
+  }));
+  
+  it('should have a "model" property',(function() {
+		expect(scope.car.model)
   }));
 
-  it('should ....', inject(function() {
-    //spec body
+  it('should have a "year" property',(function() {
+		expect(scope.car.year)
   }));
+  
+  it('should have a "price" property',(function() {
+		expect(scope.car.price)
+  }));
+  
+	it('"car.make should be "Honda"',(function() {
+		expect(scope.car.make).toBe('Honda');
+  }));
+  
+	it('"car.model should be "Civic"',(function() {
+		expect(scope.car.model).toBe('Civic');
+  }));  
+});
+
+	describe('MyCtrl2', function(){
+			var scope = {}, ctrl;
+			
+		  beforeEach(module('myApp.controllers'));
+		  
+		  beforeEach(inject(function($controller) {
+			scope = {};
+			ctrl = $controller('MyCtrl2',{$scope: scope});
+			}));
+
+		  it('should have a "cars" property',(function() {
+				expect(scope.cars)
+		  }));
+		  
+		  it('should have 6 cars',(function() {
+				expect(scope.cars.length).toBe(6)
+		  }));
+		  
+		  it('should accept anoter car"',(function() {
+				scope.cars.push({make:'Honda', model:'Civic', year:2014, price:24000});
+				expect(scope.cars.length).toBe(7)
+		  }));	
+		  		  
+		  it('2nd cars make should be "Honda"',(function() {
+				expect(scope.cars[1].make).toBe('Honda')
+		  }));			  
+  
+	});
 });
